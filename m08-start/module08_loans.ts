4aea9e9e0cb018039f2c41a4b2fe689cdc2ee49a
shawn <woltersh@outlook.de>
2023-07-18 01:27:33 +0200

    first commit

---
+++ m08-start/module08_loans.ts
@@ -0,0 +1,12 @@
+/// <reference path="module08_main.ts" />
+
+/*  TODO Create a the Loans namespace. */
+namespace Loans {
+    export interface Loan {
+        principle: number,
+        interestRate: number        //* Interest rate percentage (eg. 14 is 14%)
+    }
+    export interface ConventionalLoan extends Loan {
+        months: number      //* Total number of months
+    }
+}
